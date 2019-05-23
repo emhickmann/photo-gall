e035fed8c4b44d6198957e3586abe0610acda8e8
Emhickmann <ehickmann@turing.io>
2019-05-23 13:23:01 -0600

    added fun on click

---
+++ index.js
@@ -0,0 +1,13 @@
+var button = $('button');
+
+button.on("click", doSomething);
+
+function doSomething() {
+  var header = $('h1');
+  header.text("Doodles for Life!");
+}
+
+//
+// function changeColors() {
+//   nav.toggleClass(#735372)
+// }
